4747baceb0f35c0c064ddf7eacd48ef33e874899
ezahid101 <ezahid@oakland.edu>
2023-10-24 17:36:59 -0400

    initial

---
+++ js/app.js
@@ -0,0 +1,41 @@
+function showA() {
+  let A = document.querySelector(".imageA");
+  A.style.visibility = "visible";
+  let hideA = document.querySelector(".hideA");
+  hideA.style.visibility = "visible";
+}
+
+function hideA() {
+  let A = document.querySelector(".imageA");
+  A.style.visibility = "hidden";
+  let hideA = document.querySelector(".hideA");
+  hideA.style.visibility = "hidden";
+}
+
+function showB() {
+  let B = document.querySelector(".imageB");
+  B.style.visibility = "visible";
+  let hideB = document.querySelector(".hideB");
+  hideB.style.visibility = "visible";
+}
+
+function hideB() {
+  let B = document.querySelector(".imageB");
+  B.style.visibility = "hidden";
+  let hideB = document.querySelector(".hideB");
+  hideB.style.visibility = "hidden";
+}
+
+function showC() {
+  let C = document.querySelector(".imageC");
+  C.style.visibility = "visible";
+  let hideC = document.querySelector(".hideC");
+  hideC.style.visibility = "visible";
+}
+
+function hideC() {
+  let C = document.querySelector(".imageC");
+  C.style.visibility = "hidden";
+  let hideC = document.querySelector(".hideC");
+  hideC.style.visibility = "hidden";
+}
